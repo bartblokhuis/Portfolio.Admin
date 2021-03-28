8e204b0f34823f191f6c4aa44dedc24cc60c487e
Bart Blokhuis <info@bartblokhuis.com>
2021-03-28 20:05:13 +0200

    Implemented the message delete method

--- src/app/components/messages/delete-message/delete-message.component.ts
+++ src/app/components/messages/delete-message/delete-message.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
 import { Message } from '../../../data/Message';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-delete-message',
@@ -8,10 +10,31 @@ import { Message } from '../../../data/Message';
 export class DeleteMessageComponent implements OnInit {
 
   @Input() message: Message;
+  @Input() modalRef: NgbModalRef;
+  @Input() callBack: Function;
 
-  constructor() { }
+  private baseUrl: string;
+  private http: HttpClient;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string){
+    this.baseUrl = baseUrl;
+    this.http = http;
+  }
 
   ngOnInit(): void {
+
+  }
+
+  close(){
+    this.modalRef.close();
+  }
+
+  remove(id: number){
+    console.log(id);
+
+    this.http.delete(this.baseUrl + "Messages?messageId=" + id).subscribe(result => {
+      this.modalRef.close();
+    });
   }
 
 }
--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ModuleWithComponentFactories } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Message, MessageStatus } from '../../data/Message';
 import { DeleteMessageComponent } from './delete-message/delete-message.component';
@@ -13,16 +13,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class MessagesComponent implements OnInit {
 
-  messages: Message[] = []
+  messages: Message[] = [];
+  http: HttpClient;
+  baseUrl: string;
 
   constructor(private modalService: NgbModal, http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Message[]>(baseUrl + 'Messages').subscribe(result => {
-      this.messages = result;
-      console.log(result);
-    }, error => console.error(error));
+    this.http = http;
+    this.baseUrl = baseUrl;
    }
 
   ngOnInit(): void {
+    this.LoadMessages();
   }
 
   editMessage(message: Message) {
@@ -31,8 +32,28 @@ export class MessagesComponent implements OnInit {
   }
 
   deleteMessage(message: Message){
-    const modalRef = this.modalService.open(DeleteMessageComponent, { size: 'lg' })
+    const modalRef = this.modalService.open(DeleteMessageComponent, { size: 'lg' });
     modalRef.componentInstance.message = message;
+    modalRef.componentInstance.modalRef = modalRef;
+
+    modalRef.result.then((result => {
+      console.log(`Closed with: ${result}`);
+      this.LoadMessages();
+    }))
+    .catch((error) => {
+      console.log(`ran into error: ${error}`)
+    });
+  }
+
+  onDelete(){
+    
+  }
+
+  LoadMessages(){
+    this.http.get<Message[]>(this.baseUrl + 'Messages').subscribe(result => {
+      this.messages = result;
+      console.log(result);
+    }, error => console.error(error));
   }
 
 }
