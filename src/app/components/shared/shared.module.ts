068e63d4dc6dc1fa61639793d1f1e4c549014f59
Bart Blokhuis <bart@bartblokhuis.com>
2021-04-05 00:15:52 +0200

    Moved the sidebar component to the shared module

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent],
+  declarations: [FooterComponent, HeaderComponent, SidebarComponent],
   imports: [
     CommonModule
   ],
   exports: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
