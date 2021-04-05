d05d56348598a14789cb241b839d2b1926ffa261
Bart Blokhuis <bart@bartblokhuis.com>
2021-04-05 02:53:35 +0200

    Fixed an issue that caused menu items to not redirect

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent, SidebarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     FooterComponent,
