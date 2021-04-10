e44944c5fbc52070e2adbd994229b2d1114c707f
Bart Blokhuis <bart@bartblokhuis.com>
2021-04-10 05:38:53 +0200

    Created settings component, added new routing, added to sidebar navigation

---
+++ src/app/settings/seo-settings/seo-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seo-settings',
+  templateUrl: './seo-settings.component.html',
+  styleUrls: ['./seo-settings.component.scss']
+})
+export class SeoSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
