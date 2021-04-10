5aeaf067dbaa26d8b691396e6e39dd13d57816f0
Bart Blokhuis <bart@bartblokhuis.com>
2021-04-10 22:09:52 +0200

    Created the service and model file

--- src/app/services/settings/general-settings.service.ts
+++ src/app/services/settings/general-settings.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GeneralSettings } from '../../data/GeneralSettings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeneralSettingsService {
 
-  constructor() { }
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string ) {
+    this.baseUrl += "GeneralSettings/"
+   }
+
+   getSettings(): Observable<GeneralSettings> {
+    return this.http.get<GeneralSettings>(this.baseUrl);
+   }
+
+   saveSettings(formData: any): Observable<GeneralSettings> {
+     return this.http.post<GeneralSettings>(this.baseUrl, formData);
+   }
 }
